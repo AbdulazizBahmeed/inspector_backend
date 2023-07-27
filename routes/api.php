026281321d6f8105a5a639f2c5854bf95eebeebb
abdulaziz <gimo999@hotmail.com>
2023-07-28 00:46:11 +0300

    added the .env file

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::put('', [CompnayController::class, 'index']);
+Route::get('', [CompnayController::class, 'index']);
 Route::group(['prefix' => 'account'], function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
